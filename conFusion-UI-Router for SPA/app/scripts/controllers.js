02e0ee67e615c9e0ceb48675157ec5275e04ab0c
RohitMukherjee24 <rohitjee24@gmail.com>
2017-01-10 14:43:38 -0600

    Angular UI-Router

---
+++ conFusion-UI-Router for SPA/app/scripts/controllers.js
@@ -0,0 +1,53 @@
+'use strict';
+angular.module('confusionApp')
+
+     .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory',
+      function($scope, $stateParams, menuFactory) {
+            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
+                        $scope.dish = dish;
+                    }])
+
+    .controller('MenuController', ['$scope', 'menuFactory', function ($scope, menuFactory) {
+        this.tab = 1;
+        this.filtText = '';
+        $scope.dishes = menuFactory.getDishes();
+    }])
+
+    .controller('ContactController', ['$scope', function ($scope) {
+        $scope.feedback = { mychannel: "", firstName: "", lastName: "", agree: false, email: "" };
+        var channels = [{ value: "tel", label: "Tel." }, { value: "Email", label: "Email" }];
+        $scope.channels = channels;
+        $scope.invalidChannelSelection = false;
+    }])
+
+    .controller('FeedbackController', ['$scope', function ($scope) {
+        $scope.sendFeedback = function () {
+            console.log($scope.feedback);
+            if ($scope.feedback.agree && ($scope.feedback.mychannel === "") && !$scope.feedback.mychannel) {
+                $scope.invalidChannelSelection = true;
+                console.log('incorrect');
+            }
+            else {
+                $scope.invalidChannelSelection = false;
+                $scope.feedback = {
+                    mychannel: "", firstName: "", lastName: "",
+                    agree: false, email: ""
+                };
+                $scope.feedback.mychannel = "";
+
+                $scope.feedbackForm.$setPristine();
+                console.log($scope.feedback);
+            }
+        };
+    }])
+
+    .controller('DishController', ['$scope', function ($scope) {
+        $scope.submitComment = function () {
+            $scope.comment.date = new Date();
+            $scope.dish.comments.push($scope.comment);
+            $scope.commentForm.$setPristine();
+            $scope.comment = { author: "", rating: 5, comment: "", date: new Date() };
+        };
+    }])
+
+    ;
